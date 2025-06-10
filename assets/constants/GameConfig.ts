364dc5d86382c780e39e605f9db05dc38fb65772
llynv <linhvg19804@gmail.com>
2025-06-10 14:55:20 +0700

    feat: update GameConfig - include CandyTypes with color properties and add MaxIdleTime

--- assets/constants/GameConfig.ts
+++ assets/constants/GameConfig.ts
@@ -1,31 +1,41 @@
-const GameConfig = {
+import { Color } from 'cc';
+
+export type TileType = {
+  name: string;
+  color: Color;
+};
+
+export const GameConfig = {
   GridWidth: 8,
   GridHeight: 8,
   TileWidth: 64,
   TileHeight: 64,
+  MaxIdleTime: 10,
   CandyTypes: [
-    '1',
-    '2',
-    '3',
-    '4',
-    '5',
-    '6',
-    '7',
-    '8',
-    '9',
-    '10',
-    '11',
-    '12',
-    '13',
-    '14',
-    '15',
-    '16',
-    '17',
-    '18',
-    '19',
-    '20',
-    '21',
-  ],
+    {
+      name: 'Blue Candy',
+      // light blue
+      color: new Color(55, 78, 252, 255),
+    },
+    {
+      name: 'Green Candy',
+      color: new Color(0, 128, 0, 255),
+    },
+    {
+      name: 'Orange Candy',
+      color: new Color(255, 126, 25, 255),
+    },
+    {
+      name: 'Red Candy',
+      color: new Color(255, 0, 0, 255),
+    },
+    {
+      name: 'Yellow Candy',
+      color: new Color(255, 255, 0, 255),
+    },
+    {
+      name: 'Purple Candy',
+      color: new Color(128, 0, 128, 255),
+    },
+  ] as TileType[],
 } as const;
-
-export default GameConfig;
